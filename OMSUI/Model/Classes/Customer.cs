125dd3a00e87cb2b9ea0de468f4858fd463303c4
Naeg62306 <159005672+Naeg62306@users.noreply.github.com>
2025-11-14 07:25:11 +0100

    Klasser, Interfaces og Repo

---
+++ OMSUI/Model/Classes/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMSDesktopUI.Model.Interfaces;
+
+namespace OMSDesktopUI.Model.Classes
+{
+    public class Customer : IPerson
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Customer(int personId, string firstName, string lastName, string phoneNumber)
+        {
+            PersonId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
--- OMSUI/Model/Classes/Order.cs
+++ OMSUI/Model/Classes/Order.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OMSDesktopUI.Model.Classes;
+using OMSDesktopUI.Model.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace OMSDesktopUI.Model
 {
-    internal class Order
+    public class Order : IOrder
     {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsDraft { get; set; }
+        public int CustomerId { get; set; }
+
+        public Order(int orderId, DateTime orderDate, bool isDraft, int customerId)
+        {
+            OrderId = orderId;
+            OrderDate = orderDate;
+            IsDraft = isDraft;
+            CustomerId = customerId;
+        }
     }
 }
