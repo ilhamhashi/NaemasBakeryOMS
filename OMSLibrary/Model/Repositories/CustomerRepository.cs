576b99c506f0e74e1bb038a349839d0080d4b298
Naeg62306 <159005672+Naeg62306@users.noreply.github.com>
2025-11-25 09:57:53 +0100

    CustomerRepository

--- OMSLibrary/Model/Repositories/CustomerRepository.cs
+++ OMSLibrary/Model/Repositories/CustomerRepository.cs
@@ -2,6 +2,7 @@
 using OrderManagerLibrary.DataAccess;
 using OrderManagerLibrary.Model.Classes;
 using OrderManagerLibrary.Model.Interfaces;
+using System.Data;
 
 namespace OrderManagerLibrary.Model.Repositories;
 public class CustomerRepository : IRepository<Customer>
@@ -15,24 +16,78 @@ public class CustomerRepository : IRepository<Customer>
 
     public int Insert(Customer entity)
     {
-        throw new NotImplementedException();
+        using SqlCommand command = new SqlCommand("spCustomer_Insert", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        SqlParameter outputParam = new SqlParameter("@CustomerId", SqlDbType.Int);
+        outputParam.Direction = ParameterDirection.Output;
+
+        command.Parameters.AddWithValue("@FirstName", entity.FirstName);
+        command.Parameters.AddWithValue("@LastName", entity.LastName);
+        command.Parameters.AddWithValue("@PhoneNumber", entity.PhoneNumber);
+        command.Parameters.Add(outputParam);
+
+        _connection.Open();
+        command.ExecuteNonQuery();
+        return (int)outputParam.Value;
     }
+
     public void Update(Customer entity)
     {
-        throw new NotImplementedException();
+        using SqlCommand command = new SqlCommand("spCustomer_Update", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@CustomerId", entity.CustomerId);
+        command.Parameters.AddWithValue("@FirstName", entity.FirstName);
+        command.Parameters.AddWithValue("@LastName", entity.LastName);
+        command.Parameters.AddWithValue("@PhoneNumber", entity.PhoneNumber);
+        _connection.Open();
+        command.ExecuteNonQuery();
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        using SqlCommand command = new SqlCommand("spCustomer_Delete", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@CustomerId", id);
+        _connection.Open();
+        command.ExecuteNonQuery();
     }
     public Customer GetById(int id)
     {
-        throw new NotImplementedException();
+        Customer customer = null;
+        using SqlCommand command = new SqlCommand("spCustomer_GetById", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@CustomerId", id);
+        _connection.Open();
+
+        using SqlDataReader reader = command.ExecuteReader();
+
+        if (reader.Read())
+        {
+            customer = new Customer
+                ((int)reader["CustomerId"],
+                (string)reader["FirstName"],
+                (string)reader["LastName"],
+                (string)reader["PhoneNumber"]);
+        }
+        return customer;
     }
 
     public IEnumerable<Customer> GetAll()
     {
-        throw new NotImplementedException();
+        var customers = new List<Customer>();
+        using SqlCommand command = new("spCustomer_Insert", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        _connection.Open();
+
+        using SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            customers.Add(new Customer
+                ((int)reader["CustomerId"],
+                (string)reader["FirstName"],
+                (string)reader["LastName"],
+                (string)reader["PhoneNumber"]));
+        }
+        return customers;
     }
 }
