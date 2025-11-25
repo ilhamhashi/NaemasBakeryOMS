74c23e1a002cc40f29a937fbb8d8b48a9555de0f
Naeg62306 <159005672+Naeg62306@users.noreply.github.com>
2025-11-25 10:30:45 +0100

    DeliveryRepository

--- OMSLibrary/Model/Repositories/DeliveryRepository.cs
+++ OMSLibrary/Model/Repositories/DeliveryRepository.cs
@@ -2,6 +2,7 @@
 using OrderManagerLibrary.DataAccess;
 using OrderManagerLibrary.Model.Classes;
 using OrderManagerLibrary.Model.Interfaces;
+using System.Data;
 
 namespace OrderManagerLibrary.Model.Repositories;
 public class DeliveryRepository : IRepository<Delivery>
@@ -12,28 +13,83 @@ public class DeliveryRepository : IRepository<Delivery>
     {
         _connection = sqlDataAccess.GetSqlConnection();
     }
+
     public int Insert(Delivery entity)
     {
-        throw new NotImplementedException();
+        using SqlCommand command = new SqlCommand("spDelivery_Insert", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        SqlParameter outputParam = new SqlParameter("@CollectionId", SqlDbType.Int);
+        outputParam.Direction = ParameterDirection.Output;
+
+        command.Parameters.AddWithValue("@CollectionDate", entity.CollectionDate);
+        command.Parameters.AddWithValue("@OrderId", entity.OrderId);
+        command.Parameters.AddWithValue("@Neighborhood", entity.Neighborhood);
+        command.Parameters.Add(outputParam);
+
+        _connection.Open();
+        command.ExecuteNonQuery();
+        return (int)outputParam.Value;
     }
 
     public void Update(Delivery entity)
     {
-        throw new NotImplementedException();
+        using SqlCommand command = new SqlCommand("spDelivery_Update", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@CollectionId", entity.CollectionId);
+        command.Parameters.AddWithValue("@CollectionDate", entity.CollectionDate);
+        command.Parameters.AddWithValue("@OrderId", entity.OrderId);
+        command.Parameters.AddWithValue("@Neighborhood", entity.Neighborhood);
+        _connection.Open();
+        command.ExecuteNonQuery();
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        using SqlCommand command = new SqlCommand("spDelivery_Delete", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@CollectionId", id);
+        _connection.Open();
+        command.ExecuteNonQuery();
+    }
+    public Delivery GetById(int id)
+    {
+        Delivery delivery = null;
+        using SqlCommand command = new SqlCommand("spDelivery_GetById", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@CollectionId", id);
+        _connection.Open();
+
+        using SqlDataReader reader = command.ExecuteReader();
+
+        if (reader.Read())
+        {
+            delivery = new Delivery
+                ((int)reader["CollectionId"],
+                (DateTime)reader["CollectionDate"],
+                (int)reader["OrderId"],
+                (string)reader["Neighborhood"]);
+        }
+        return delivery;
     }
 
     public IEnumerable<Delivery> GetAll()
     {
-        throw new NotImplementedException();
-    }
+        var deliveries = new List<Delivery>();
+        using SqlCommand command = new("spDelivery_Insert", _connection);
+        command.CommandType = CommandType.StoredProcedure;
+        _connection.Open();
 
-    public Delivery GetById(int id)
-    {
-        throw new NotImplementedException();
+        using SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            deliveries.Add(new Delivery
+            (
+                (int)reader["CollectionId"],
+                (DateTime)reader["CollectionDate"],
+                (int)reader["OrderId"],
+                (string)reader["Neighborhood"]
+            ));
+        }
+        return deliveries;
     }
 }
