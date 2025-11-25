703e84a910977f3cf1d07a574c8f69664859298f
IlhamHashi <iaha71375@edu.ucl.dk>
2025-11-25 10:46:42 +0300

    orderrepository unit tests

--- OMSLibrary.Tests/OrderRepositoryTests.cs
+++ OMSLibrary.Tests/OrderRepositoryTests.cs
@@ -38,5 +38,69 @@ public sealed class OrderRepositoryTests
         var retrievedOrder = _orderRepository.GetById(order.OrderId);
         Assert.IsNotNull(retrievedOrder);
         Assert.AreEqual(order.CustomerId, retrievedOrder.CustomerId);
+        Assert.AreEqual(order.Status, retrievedOrder.Status);
+    }
+
+    [TestMethod]
+    public void UpdateOrder_ShouldUpdateOrderSuccesfully()
+    {
+        // Arrange
+        var order = new Order
+        (
+            DateTime.Now,
+            OrderStatus.Confirmed,
+            1
+        );
+
+        // Act
+        order.OrderId = _orderRepository.Insert(order);
+
+        // Assert
+        var retrievedOrder = _orderRepository.GetById(order.OrderId);
+        Assert.IsNotNull(retrievedOrder);
+        Assert.AreEqual(order.CustomerId, retrievedOrder.CustomerId);
+        Assert.AreEqual(order.Status, retrievedOrder.Status);
+    }
+
+    [TestMethod]
+    public void DeleteOrder_ShouldDeleteOrderSuccesfully()
+    {
+        // Arrange
+        var order = new Order
+        (
+            DateTime.Now,
+            OrderStatus.Confirmed,
+            1
+        );
+
+        // Act
+        order.OrderId = _orderRepository.Insert(order);
+
+        // Assert
+        var retrievedOrder = _orderRepository.GetById(order.OrderId);
+        Assert.IsNotNull(retrievedOrder);
+        Assert.AreEqual(order.CustomerId, retrievedOrder.CustomerId);
+        Assert.AreEqual(order.Status, retrievedOrder.Status);
+    }
+
+    [TestMethod]
+    public void GetById_ShouldGetOrderSuccesfully()
+    {
+        // Arrange
+        var order = new Order
+        (
+            DateTime.Now,
+            OrderStatus.Confirmed,
+            1
+        );
+
+        // Act
+        order.OrderId = _orderRepository.Insert(order);
+
+        // Assert
+        var retrievedOrder = _orderRepository.GetById(order.OrderId);
+        Assert.IsNotNull(retrievedOrder);
+        Assert.AreEqual(order.CustomerId, retrievedOrder.CustomerId);
+        Assert.AreEqual(order.Status, retrievedOrder.Status);
     }
 }
